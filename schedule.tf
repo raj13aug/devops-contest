e2dbad385dd3a73982499a9f76884d1c9fd4881f
nataraj.ramamoorthy <nataraj.ramamoorthy13@gmail.com>
2024-04-26 11:59:49 +0530

    ffgdsg

--- schedule.tf
+++ schedule.tf
@@ -2,19 +2,6 @@ data "google_project" "this_project" {
   project_id = var.project_id
 }
 
-resource "google_compute_resource_policy" "uptime_schedule" {
-  name        = "uptime-schedule"
-  description = "Keep instances shut down during nighttime to save money"
-  instance_schedule_policy {
-    vm_start_schedule {
-      schedule = var.uptime_schedule["start"]
-    }
-    vm_stop_schedule {
-      schedule = var.uptime_schedule["stop"]
-    }
-    time_zone = var.uptime_schedule["time_zone"]
-  }
-}
 
 resource "google_project_iam_custom_role" "start_stop" {
   role_id     = "instanceScheduler"
@@ -31,4 +18,18 @@ resource "google_project_iam_member" "member" {
 
 resource "time_sleep" "wait_30_seconds" {
   create_duration = "30s"
+}
+
+resource "google_compute_resource_policy" "uptime_schedule" {
+  name        = "uptime-schedule"
+  description = "Keep instances shut down during nighttime to save money"
+  instance_schedule_policy {
+    vm_start_schedule {
+      schedule = var.uptime_schedule["start"]
+    }
+    vm_stop_schedule {
+      schedule = var.uptime_schedule["stop"]
+    }
+    time_zone = var.uptime_schedule["time_zone"]
+  }
 }
